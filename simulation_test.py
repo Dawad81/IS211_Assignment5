cfe5db46c36056a96dd4b807832fce50f7e3196e
Dawad81 <dennis.awad83@spsmail.cuny.edu>
2019-03-02 22:11:18 -0500

    more trys to get simulateManyServer() working in simulation test file

--- simulation_test.py
+++ simulation_test.py
@@ -88,40 +88,52 @@ def simulateManyServer(downloadcsvfile,num_server):
     serverdict={}
     server_list = []
     for num in range(0, int(num_server)):
-        server_list.append(num)
-    for server in server_list:
+        server_list.append([])
+    #print server_list
+    #for server in server_list:
         #serverdict[server]= simulateOneServer(urllib2.urlopen(url))
-        serverdict[server]= None
+        #serverdict[server]= []
     reader = csv.reader(downloadcsvfile)
     server = Server()
     queue = Queue()
     waiting_times = []
-    for current_second in reader:
-        second_requested = int(current_second[0])
-        time_needed = int(current_second[2])
-        request = Request(second_requested, time_needed)
-        queue.enqueue(request)
-        
-        if (not server.busy()) and (not queue.is_empty()):
-            next_task = queue.dequeue()
-            waiting_times.append(next_task.wait_time(second_requested))
-            server.start_next(next_task)
+    #for current_second in reader:
+    for item in server_list:
+        for current_second in reader:
+            #for item in server_list:
+            second_requested = int(current_second[0])
+            time_needed = int(current_second[2])
+            request = Request(second_requested, time_needed)
+            queue.enqueue(request)
+            #roundrobin=request[::int(num_server)]
+            #for item in server_list:
+                #for i in range(num_server)
+                    #queue_request = queue.enqueue(request)
+                #server_list[::num_server].append(queue_request)
+                    #item.append(queue_request)
+            #print server_list
 
-        server.tick()
-    average_wait = sum(waiting_times) / len(waiting_times)
-    print"Average Wait %6.2f secs %3d tasks remaining." %(average_wait, queue.size())
+            if (not server.busy()) and (not queue.is_empty()):
+                next_task = queue.dequeue()
+                waiting_times.append(next_task.wait_time(second_requested))
+                server.start_next(next_task)
 
-
+            server.tick()
+                
+        average_wait = sum(waiting_times) / len(waiting_times)
+        wait = "Average Wait %6.2f secs %3d tasks remaining." %(average_wait, queue.size())
+        item.append(wait)
+    print server_list
     #for server in server_list[::int(num_server)]:
         #serverdict[server]= queue.enqueue(request)
-#[::int(nums_server)]
-    print serverdict
-#for i in range(team):
-#    teamlist=players[i::team]
-#    game.append(teamlist)
+#[::int(num_server)]
+    #print serverdict
+#for i in range(num_server):
+#    list=current_second[i::num_server]
+#    item.append(list)
 #    return game
 
-test1 = simulateManyServer(urllib2.urlopen(url), 3)
+test1 = simulateManyServer(urllib2.urlopen(url), 2)
 
 
 def main():
